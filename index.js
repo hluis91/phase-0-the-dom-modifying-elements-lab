34a01cb68431baad2e47e77be21adaba200bcc0a
Luis Hernandez <lshrnndz6@gmail.com>
2022-12-29 15:14:29 -0800

    commit tree 4df8150fc3bc53955e8f149ba5502302336d946b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.textContent = 'Luis is the champion'
+
+
+
+
+
+
+
+
+
+
+
+
